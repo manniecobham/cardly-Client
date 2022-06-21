e622bcca5cb2cb13850cd326f23f221158a70dbd
Emmanuel Cobham <manniecobham@yahoo.com>
2022-06-21 16:00:41 +0200

    Login api

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ function Login() {
 
     const login = () => {
         const data = { username: username, password: password }
-        axios.post("https://mannie-blog.herokuapp.com/login", data).then((response) => {
+        axios.post("https://mannie-blog.herokuapp.com/auth/login", data).then((response) => {
             if (response.data.error) {
                 alert(response.data.error)
             } else {
